4bc4d9183010f6237707dd07e5cf3e5d9151876e
DerekHXZ <derekhe@gmail.com>
2013-04-07 09:46:56 -0400

    deploy

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 from foursquare import Foursquare
 
-base_url="http://localhost:5000"
+base_url="http://eatnew.hsingchou.info"
 
 def makeClient():
     return Foursquare(client_id = "ATQBBI1MMQJCTD43UXTO5IXK0CXPUPCUH5M4DZWOS1LL3013",
