ccb8e4bc925fd5def9d3c3b2cf907efcee0791a2
DerekHXZ <derekhe@gmail.com>
2013-04-07 02:52:33 -0400

    change

---
+++ eatnew.py
@@ -0,0 +1,38 @@
+# imports
+from init import makeClient
+import getdata
+import getproducts
+import synonyms
+from flask import Flask, redirect, session, request, url_for, render_template, flash
+from foursquare import Foursquare
+
+app = Flask(__name__)
+
+@app.route("/")
+def login_page():
+    return render_template("login.html")
+
+@app.route("/auth")
+def foursquare_connect():
+    return redirect(makeClient().oauth.auth_url());
+
+@app.route("/login")
+def redirect_page():
+    client = makeClient()
+    code = request.args.getlist("code")
+    access_token = client.oauth.get_token(str(code[0]))
+    session['access_token'] = access_token
+    return redirect("/home")
+
+@app.route("/home")
+def home_page():
+    data = getdata.getData()
+    products = getproducts.getProducts(data)
+    for listing in products:
+        print(listing.IMAGE_URL)
+    return render_template("main_page.html", products=products)
+
+app.debug = True
+app.secret_key = "/x85/xfe/x98j/xc8FQb-/x88/xaf/x87/xda/xed/xba/n/x1dk/xbb//0b/xb06/xd2/x87"
+if __name__ == "__main__":
+    app.run()
--- init.py
+++ init.py
@@ -3,7 +3,7 @@ from foursquare import Foursquare
 base_url="http://localhost:5000"
 
 def makeClient():
-    return Foursquare(client_id = "3GZG0BE3SQSWRKGCXXSEJD4FM2TWGXTQOFQUWOKCQ3ZILXEP",
-            client_secret = "EXTDHANAPBAR3PDAGJU2DLLZFJPPH1KZYESMY00LXZCVTEBW",
-            redirect_uri = base_url + "/auth2")
+    return Foursquare(client_id = "ATQBBI1MMQJCTD43UXTO5IXK0CXPUPCUH5M4DZWOS1LL3013",
+            client_secret = "L5T1HRSW2LYUQTNCSF12PZXA1TVT2HZDSD5Y4ZCPBPEIQ1RI",
+            redirect_uri = base_url + "/login")
 
